830b2cf5b3f7c1ba61ddc1a8616de27f3c42f304
AbyChan <crystal.tyan@gmail.com>
2016-07-22 23:36:13 +0800

    initial commit

---
+++ service/error.js
@@ -0,0 +1,9 @@
+'use strict';
+
+export class ErrorParamsError extends Error {
+  constructor(message) {
+    super(message);
+    this.name = this.constructor.name;
+    this.message = message;
+  }
+}
