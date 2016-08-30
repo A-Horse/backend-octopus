98d8d60c69e3d64eeb343725fd376d22122f5312
fwchen <me@chenfangwei.com>
2016-08-30 14:26:52 +0800

    refactor code structure

--- app.js
+++ app.js
@@ -1,13 +1,8 @@
-'use strict';
+import express from 'express';
+import morgan from 'morgan';
+import http from 'http';
 
-let express = require('express'),
-    morgan = require('morgan'),
-    http = require('http');
-
-
-let app = express(),
-    setting = require('./setting');
-
+const app = express();
 
 app.set('view engine', 'ejs');
 app.set('views', '../views');
@@ -21,22 +16,19 @@ app.use(require('cookie-parser')());
 
 
 import {UserRouter} from './route/user.js';
-app.use('/api', UserRouter);
-
 import {TaskWallRouter} from './route/task/task-wall';
-app.use('/api', TaskWallRouter);
-
 import {TaskListRouter} from './route/task/task-list';
-app.use('/api', TaskListRouter);
-
 import {TaskCardRouter} from './route/task/task-card';
-app.use('/api', TaskCardRouter);
-
 import {StatusErrorHandleMiddle} from './route/middle/error-handle';
+
+app.use('/api', UserRouter);
+app.use('/api', TaskWallRouter);
+app.use('/api', TaskListRouter);
+app.use('/api', TaskCardRouter);
 app.use(StatusErrorHandleMiddle);
 
 function startHttp() {
-  let server = http.createServer(app);
+  const server = http.createServer(app);
   server.listen(5000);
 }
 
