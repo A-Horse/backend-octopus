c831565b6cec21d486e28c34e3f08891abf912aa
AbyChan <crystal.tyan@gmail.com>
2016-09-05 21:57:19 +0800

    add userId for table goal

--- db/table/goal.js
+++ db/table/goal.js
@@ -2,6 +2,7 @@ import {knex} from '../table';
 
 export const createPromise = knex.schema.createTableIfNotExists('goal', function (table) {
   table.increments();
+  table.integer('userId');
   table.string('title');
   table.string('content');
   table.timestamps();
