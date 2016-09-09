4b5b319ea88c77307fcf291a66eee399aeb56974
AbyChan <crystal.tyan@gmail.com>
2016-09-09 23:33:49 +0800

    idea table knex schema

---
+++ db/table/idea.js
@@ -0,0 +1,14 @@
+import {knex} from '../table';
+
+const TableName = 'idea';
+
+export const createPromise = knex.schema.createTableIfNotExists(TableName, function (table) {
+  table.increments();
+  table.integer('userId');
+  table.string('title');
+  table.string('content');
+  table.timestamps();
+});
+
+export const dropPromise = knex.schema.dropTableIfExists(TableName);
+
