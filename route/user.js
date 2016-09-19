0c7d1250673a7a2c99163f8101eb6d220d0f0406
fwchen <me@chenfangwei.com>
2016-09-19 11:23:25 +0800

    refactor user.js route

--- route/user.js
+++ route/user.js
@@ -5,6 +5,8 @@ import {authJwt} from './middle/jwt';
 import {signJwt} from '../service/auth';
 import {checkIsEmailIdentity} from '../util';
 import {makeGravatarUrl} from '../service/gravator.js';
+import {validateRequest} from '../service/validate';
+import {JWT_KEY} from '../constant';
 
 const UserRouter = express.Router();
 
@@ -30,17 +32,17 @@ UserRouter.get('/login', authJwt, (req, res, next) => {
 });
 
 UserRouter.post('/logout', authJwt, (req, res) => {
-  
+  res.status(204).send();
 });
 
-UserRouter.post('/login', (req, res, next) => {
+UserRouter.post('/signin', (req, res, next) => {
   const email = req.body.email,
-      password = req.body.password;
+        password = req.body.password;
 
   if (!email && !password) {
     return res.status(400).send();
   }
-  
+
   const creds = {email: email, password: password};
   
   User.authUser(creds).then(user => {
@@ -51,14 +53,14 @@ UserRouter.post('/login', (req, res, next) => {
       jwt: signJwt({user: user}),
       user: user
     });
-  }).catch(error => {
-    res.status(500).send();
-    throw error;
-  });
+  }).catch(next);
 });
 
-UserRouter.post('/sign-up', (req, res, next) => {
-  let {username, password, email} = req.body;
+UserRouter.post('/signup', (req, res, next) => {
+  validateRequest(req.body, 'username', ['required']);
+  validateRequest(req.body, 'password', ['required']);
+  validateRequest(req.body, 'email', ['required']);
+  const {username, password, email} = req.body;
   
   User.createUser({
     username,
@@ -66,13 +68,13 @@ UserRouter.post('/sign-up', (req, res, next) => {
     email
   }).then((user) => {
     user.save().then((user) => {
-      let json = user.omit('password');
+      const json = user.omit('password');
       const token = signJwt({user: json});
       
-      res.header('jwts-token', token);
+      res.header(JWT_KEY, token);
       res.status(201).send(json);
     });
-  }).catch(error => {throw error});
+  }).catch(next);
 });
 
 export {UserRouter};
