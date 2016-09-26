4c67f7ff2634157671b59d4f058b58c3f26ea7f6
fwchen <me@chenfangwei.com>
2016-09-27 00:11:34 +0800

    todo-task-link table

---
+++ db/table/todo-task-link.js
@@ -0,0 +1,13 @@
+import {knex} from '../table';
+
+const TableName = 'todo-task-link';
+
+export const createPromise = knex.schema.createTableIfNotExists(TableName, function (table) {
+  table.increments();
+  table.integer('todoId');
+  table.string('taskCardLink');
+  table.timestamps();
+});
+
+export const dropPromise = knex.schema.dropTableIfExists(TableName);
+
