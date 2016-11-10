18d46d1cf01e12a5e385b52f44526127dedfe60f
fwchen <me@chenfangwei.com>
2016-11-10 10:52:58 +0800

    password-generator tool

--- tool/password-generator.js
+++ tool/password-generator.js
@@ -1,15 +1,17 @@
 import bcrypt from 'bcryptjs';
 
 
-function main() {
-  if (!process.argv[2]) {
-    return console.warning('Please input raw password.');
+async function main() {
+  const input = process.argv[2];
+  if (!input) {
+    return console.warn('Please input raw password.');
   }
-
+  
   bcrypt.genSalt(10, (error, salt) => {
-    bcrypt.hash(process.argv[2], salt, (error, hash) => {
-      
+    bcrypt.hash(input, salt, (error, hash) => {
+      console.log(hash);
     })
   });
 }
 
+main();
