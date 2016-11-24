dc140774b4ce693f731a9202d5eb8a0db1a29e9d
fwchen <me@chenfangwei.com>
2016-11-24 15:06:46 +0800

    date for comment

--- model/task-card-comment.js
+++ model/task-card-comment.js
@@ -2,5 +2,8 @@ import {bookshelf} from '../db/bookshelf.js';
 import {UserModel} from './user';
 
 export const TaskCardCommentModel = bookshelf.Model.extend({
-  tableName: 'task-card-comment'
+  tableName: 'task-card-comment',
+  creater: function() {
+    return this.belongsTo(UserModel, 'createrId');
+  }
 });
