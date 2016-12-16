5494d983e728d4301d6361bf718aca1315a87dc0
fwchen <crystal.tyan@gmail.com>
2016-12-16 14:53:38 +0800

    backup.sh

--- db/backup.sh
+++ db/backup.sh
@@ -1,7 +1,15 @@
 #!/bin/bash
 
-$ENV='DEV'
-
+DEV='DEV'
+if test $1=='dev'
+then
+    ENV='DEV'
+else if test $1=='product'
+     then
+         ENV='PRODUCT'
+     fi
+fi
+DATE_STR=`date +%Y-%m-%d_%H_%M_%S`
 mkdir -p data-backup
-sqlite3 db-DEV.sqlite ".backup ./data-backup/bk_database.sq3.bak"
-# date +"%m-%d-%y_%T"
+sqlite3 db-$ENV.sqlite ".backup ./data-backup/db-"$ENV"-"$DATE_STR".sq3.bak"
+
