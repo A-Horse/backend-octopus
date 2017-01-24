0e843949bd26acb303113199f47cb5d1239fee79
fwchen <crystal.tyan@gmail.com>
2017-01-25 00:04:56 +0800

    update

--- route/file.js
+++ route/file.js
@@ -2,6 +2,7 @@ import express from'express';
 import {AccessLimitError, NotFoundError} from '../service/error';
 import {authJwt} from './middle/jwt';
 import {checkIsEmailIdentity} from '../util';
+import R from 'ramda';
 
 import fs from 'fs';
 import md5 from 'blueimp-md5';
@@ -12,9 +13,10 @@ var multipartMiddleware = multipart();
 const FileRouter = express.Router();
 
 FileRouter.post('/file', multipartMiddleware, (req, res, next) => {
-  const image = req.body.playload.replace(/^data:image\/\w+;base64,/, '');
-  fs.writeFile('hi', image, 'base64', function(err) {
-
+  const imageURLData = req.body.playload.replace(/^data:image\/\w+;base64,/, '');
+  const hash = md5(imageURLData + Date.now()).substring(0, 20);
+  const filename = R.compose(R.join('-'), R.splitEvery(5))(hash);
+  fs.writeFile(filename, imageURLData, 'base64', function(err) {
     res.json({hi: 'hi'});
   });
 });
