0ad99b59df5a682e39646bc48c5ff1a9fbd64d1e
fwchen <crystal.tyan@gmail.com>
2017-06-18 21:15:46 +0800

    initial commit

--- mirco/scheduling.js
+++ mirco/scheduling.js
@@ -1,6 +1,13 @@
 import schedule from 'node-schedule';
-
+import { TodoModel } from '../model/todo';
 
 const scheduleJob = schedule.scheduleJob('0 * * *', () => {
   console.log('The answer to life, the universe, and everything!');
+
+
 });
+
+
+const xx = function() {
+  TodoModel.where()
+};
