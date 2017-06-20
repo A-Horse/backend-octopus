a90c0710149ec410a2b24315c9b979c51dcd737d
fwchen <crystal.tyan@gmail.com>
2017-06-20 11:39:31 +0800

    initial commmit

--- mirco/scheduling.js
+++ mirco/scheduling.js
@@ -4,40 +4,12 @@ import { TodoRepeatModel } from '../model/todo-repeat';
 import moment from 'moment';
 import R from 'ramda';
 import { scheduleLogger } from '../log';
-console.log("scheduleLogger = ", scheduleLogger);
+import { todoEveryDayBegin } from '../schedule/todo';
 
 const scheduleJob = schedule.scheduleJob('0 * * *', () => {
-  console.log('start schedule', new moment());
+  scheduleLogger.info('schedule', new moment().format('YYYY-MM-DD'));
 
-  processTodoRepeat();
+  todoEveryDayBegin();
 });
 
-function filterTodo(todo) {
-  return Math.ceil((-todo.get('created_at') + new Date('2017-6-21').getTime()) / (60 * 60 * 24 * 1000)) % todo.get('repeat') === 0;
-}
-
-async function processTodoRepeat() {
-  try {
-    const todos = await TodoModel.query(
-      'where', 'repeat', '!=', 'null'
-    ).fetchAll();
-    const filteredTodos = todos.filter(filterTodo);
-    await Promise.all(filteredTodos.map(async (todo) => {
-      scheduleLogger.info('todo id', todo.id, 'backlog to repeat table.')
-      new TodoRepeatModel({
-        todoId: todo.id,
-        isDone: todo.get('isDone'),
-        doneTime: todo.get('doneTime'),
-        created_at: new Date().getTime()
-      }).save();
-      await todo.save({
-        isDone: null,
-        doneTime: null
-      });
-    }));
-  } catch (error) {
-    console.error(error);
-  }
-};
-
-processTodoRepeat();
+todoEveryDayBegin();
