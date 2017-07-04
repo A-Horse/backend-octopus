0bf6905df62afcd4111d4c56dbbdb2629721bd7a
fwchen <crystal.tyan@gmail.com>
2017-07-04 20:23:52 +0800

    initial commit

--- db/table/todo.js
+++ db/table/todo.js
@@ -1,22 +1,25 @@
-import {knex} from '../table';
+import { knex } from '../table';
 
 export const TableName = 'todo';
 
-export const createPromise = knex.schema.createTableIfNotExists(TableName, function (table) {
-  table.increments();
-  table.integer('userId');
-  table.integer('todoBoxId');
-  table.string('content');
-  table.string('remark');
-  table.boolean('isDone');
-  table.boolean('isStar');
-  table.string('repeat');
-  table.dateTime('deadline');
-  table.dateTime('noticeTime');
-  table.dateTime('doneTime');
-  table.string('tags');
-  table.boolean('isDelete'); // *
-  table.timestamps();
-});
+export const createPromise = knex.schema.createTableIfNotExists(
+  TableName,
+  function(table) {
+    table.increments();
+    table.integer('userId');
+    table.integer('todoBoxId');
+    table.string('content');
+    table.string('remark');
+    table.boolean('isDone');
+    table.boolean('isStar');
+    table.string('repeat');
+    table.dateTime('deadline');
+    table.dateTime('noticeTime');
+    table.dateTime('doneTime');
+    table.string('tags');
+    table.boolean('isDelete');
+    table.timestamps();
+  }
+);
 
 export const dropPromise = knex.schema.dropTableIfExists(TableName);
--- route/middle/auth.js
+++ route/middle/auth.js
@@ -5,19 +5,32 @@ export function authTaskPermissions(req, res, next) {
   return next();
 }
 
-
 export async function taskBoardGroup(req, res, next) {
   const { jw } = req;
-
-};
+}
 
 export async function tdPermissions(req, res, next) {
   try {
     // TODO: todo creater => todo box => if todo box => check todo box group
     const { todoId } = req.params;
     const { jw } = req;
-    const todo = await TodoModel.where({id: todoId}).fetch();
-    if ( todo.get('userId') === jw.user.id ) {
+    const todo = await TodoModel.where({ id: todoId }).fetch();
+    if (todo.get('userId') === jw.user.id) {
+      return next();
+    }
+    return next(new AccessLimitError());
+  } catch (error) {
+    return next(error);
+  }
+}
+
+export async function wikiPermissions(req, res, next) {
+  try {
+    // TODO: todo creater => todo box => if todo box => check todo box group
+    const { todoId } = req.params;
+    const { jw } = req;
+    const todo = await TodoModel.where({ id: todoId }).fetch();
+    if (todo.get('userId') === jw.user.id) {
       return next();
     }
     return next(new AccessLimitError());
