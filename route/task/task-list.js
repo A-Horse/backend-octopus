7c182b80f45046310e58ba92f1c48728deaf1c31
fwchen <me@chenfangwei.com>
2017-09-15 11:49:02 +0800

    rename url and format

--- route/task/task-list.js
+++ route/task/task-list.js
@@ -1,9 +1,9 @@
 import express from 'express';
-import {authJwt} from '../middle/jwt';
-import {TaskList, TaskListModel} from '../../model/task-list';
-import {Group} from '../../model/group';
-import {AccessLimitError, NotFoundError} from '../../service/error';
-import {validateRequest} from '../../service/validate';
+import { authJwt } from '../middle/jwt';
+import { TaskList, TaskListModel } from '../../model/task-list';
+import { Group } from '../../model/group';
+import { AccessLimitError, NotFoundError } from '../../service/error';
+import { validateRequest } from '../../service/validate';
 import R from 'fw-ramda';
 
 const TaskListRouter = express.Router();
@@ -11,61 +11,83 @@ const TaskListRouter = express.Router();
 // TaskListRouter.use(authJwt);
 
 TaskListRouter.get('/task-wall/:wallId/list/:listId', (req, res) => {
-  const {listId} = this.params;
-  TaskList.getModel().where({id: listId}).fetch().then(taskList => {
-    if( !taskList ) throw new NotFoundError('not found this task list');
-    return res.send(taskList)
-  }).catch(error => {throw error});
+  const { listId } = this.params;
+  TaskList.getModel()
+    .where({ id: listId })
+    .fetch()
+    .then(taskList => {
+      if (!taskList) throw new NotFoundError('not found this task list');
+      return res.send(taskList);
+    })
+    .catch(error => {
+      throw error;
+    });
 });
 
-TaskListRouter.post('/task-wall/:wallId/list', (req, res) => {
+TaskListRouter.post('/task-board/:boardId/track', (req, res) => {
   validateRequest(req.body, 'name', ['required']);
-  const {jw} = req;
-  const {wallId} = req.params;
+  const { jw } = req;
+  const { boardId } = req.params;
 
-  Group.getModel().where({
-    taskWallId: wallId,
-    userId: jw.user.id
-  }).fetch().then(async (access) => {
-    if( !access ) throw new AccessLimitError('can access this task wall');
+  Group.getModel()
+    .where({
+      taskWallId: boardId,
+      userId: jw.user.id
+    })
+    .fetch()
+    .then(async access => {
+      // TODO 错误抛不出去
+      if (!access) throw new AccessLimitError('can access this task wall');
 
-    const existNumber = await TaskListModel.where({taskWallId: wallId}).count();
-    new TaskList({taskWallId: wallId, name: req.body.name}).model.save({index: existNumber}).then(taskList => {
-      res.status(201).send()
+      const existNumber = await TaskListModel.where({ taskWallId: boardId }).count();
+      const savedTrack = await new TaskListModel().save({
+        index: existNumber,
+        taskWallId: boardId,
+        name: req.body.name
+      });
+      res.status(201).send({ ...savedTrack.serialize(), cards: [] });
     });
-  })
 });
 
 TaskListRouter.patch('/task-board/:boardId/track/index', async (req, res) => {
-  const {boardId} = req.params;
-  const {trackIndexs} = req.body;
-  const result = await Promise.all(trackIndexs.map(async (trackIndex) => {
-    const track = await TaskListModel.forge({id: trackIndex.id}).save({index: trackIndex.index});
-    return track;
-  }));
+  const { boardId } = req.params;
+  const { trackIndexs } = req.body;
+  const result = await Promise.all(
+    trackIndexs.map(async trackIndex => {
+      const track = await TaskListModel.forge({ id: trackIndex.id }).save({
+        index: trackIndex.index
+      });
+      return track;
+    })
+  );
   res.status(200).json(result);
 });
 
 TaskListRouter.patch('/task-board/:wallId/list/:listId', async (req, res) => {
-  const {listId} = req.params;
+  const { listId } = req.params;
   const info = req.body;
   // TODO 检查是否存在
-  const track = await new TaskListModel({id: listId}).fetch();
+  const track = await new TaskListModel({ id: listId }).fetch();
   if (!track) throw new NotFoundError('can not found this task track');
   const newTrack = track.save(info);
   return res.send(newTrack);
 });
 
-TaskListRouter.delete('/task-wall/:wallId/list/:listId', (req, res) => {
-  const {wallId, listId} = req.params;
-  const {jw} = req;
-  Group.getModel().where({taskWallId: wallId, userId: jw.user.id}).fetch()
-    .then(access => {
-      if (!access) throw new AccessLimitError('can access this task wall');
-      new TaskListModel({id: listId}).bundleDelete().then(() => {
+// TODO rename
+TaskListRouter.delete('/task-board/:wallId/track/:listId', (req, res) => {
+  const { wallId, listId } = req.params;
+  const { jw } = req;
+  Group.getModel().where({ taskWallId: wallId, userId: jw.user.id }).fetch().then(access => {
+    if (!access) throw new AccessLimitError('can access this task wall');
+    new TaskListModel({ id: listId })
+      .bundleDelete()
+      .then(() => {
         res.status(201).send();
-      }).catch(error => {throw error});
-    });
+      })
+      .catch(error => {
+        throw error;
+      });
+  });
 });
 
-export {TaskListRouter};
+export { TaskListRouter };
