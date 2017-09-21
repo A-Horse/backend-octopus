887cbc8cedb9238ee4f33ec935a6987870019da0
fwchen <crystal.tyan@gmail.com>
2017-09-21 21:54:57 +0800

    update

--- route/todo/todo-box.js
+++ route/todo/todo-box.js
@@ -1,6 +1,7 @@
 import express from 'express';
 import { authJwt } from '../middle/jwt';
 import { TodoBoxModel } from '../../model/todo-box';
+import { TodoModel } from '../../model/todo';
 
 const TodoBoxRouter = express.Router();
 
@@ -11,9 +12,22 @@ TodoBoxRouter.post('/todo-box', authJwt, async (req, res, next) => {
       creatorId: jw.user.id,
       ownerId: jw.user.id,
       name: req.body.name,
-      type: req.body.type
+      type: req.body.type || 'normal'
     }).save();
     res.status(201).json(todoBox);
+  } catch (error) {
+    next(error);
+  }
+});
+
+TodoBoxRouter.get('/todo-box/:todoBoxId', authJwt, async (req, res, next) => {
+  const { jw } = req;
+  const { todoBoxId } = req.params;
+  try {
+    const todos = await new TodoModel()
+      .query({ where: { isDelete: null, userId: jw.user.id, todoBoxId: todoBoxId } })
+      .fetchAll();
+    res.json(todos);
   } catch (error) {
     next(error);
   }
@@ -32,9 +46,9 @@ TodoBoxRouter.delete('/todo-box', authJwt, async (req, res, next) => {
   }
 });
 
-TodoBoxRouter.get('/user/:userId/todo-box', authJwt, async (req, res, next) => {
+TodoBoxRouter.get('/user/:userId/todo-box', authJwt, async (req, res) => {
   const { jw } = req;
-  const todoBoxs = await TodoBoxModel.where({ userId: jw.user.id }).fetchAll();
+  const todoBoxs = await TodoBoxModel.where({ creatorId: jw.user.id }).fetchAll();
   res.json(todoBoxs);
 });
 
