cae89662569b12b37e0c04da405fb867d92b7e85
fwchen <me@chenfangwei.com>
2017-10-02 23:10:53 +0800

    great refactor

--- mirco-service/task/task-board.router.js
+++ mirco-service/task/task-board.router.js
@@ -40,6 +40,21 @@ TaskBoardRouter.get('/user/:userId/task-board', authJwt, async (req, res, next) 
   }
 });
 
+// TaskBoardModel.fetchAll().then(collection => {
+//   collection.forEach(taskBoard => {
+//     console.log(taskBoard);
+//     console.log(taskBoard.ownerId);
+//     new TaskAccessModel({
+//       userId: taskBoard.get('ownerId'),
+//       boardId: taskBoard.get('id'),
+//       level: 5,
+//       created_at: new Date()
+//     })
+//       .save()
+//       .then();
+//   });
+// });
+
 TaskBoardRouter.get('/task-board/:id/verbose', authJwt, async (req, res, next) => {
   const { id } = req.params;
   const board = await TaskWall.getModel()
