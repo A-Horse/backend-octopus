35b0de97b4f33497e95e3d302a984b1e9e14153e
fwchen <me@chenfangwei.com>
2018-04-17 10:08:25 +0800

    fix wrong import

--- src/model/task-wall.js
+++ src/model/task-wall.js
@@ -1,16 +1,12 @@
-import {
-  bookshelf
-} from '../db/bookshelf.js';
+import { bookshelf } from '../db/bookshelf.js';
 
-import {TaskListModel} from './task-list';
+import { TaskListModel } from './task-track';
 import { GroupModel } from './group';
 
 export const TaskBoardModel = bookshelf.Model.extend({
   tableName: 'task-board',
-  cards: function() {
-
-  },
-  tracks: function(){
+  cards: function() {},
+  tracks: function() {
     return this.hasMany(TaskListModel, 'taskWallId');
   },
   group: function() {
@@ -18,12 +14,12 @@ export const TaskBoardModel = bookshelf.Model.extend({
   }
 });
 
-import {Group} from './group';
-import {TaskList, DEFAULT_LIST_NAME} from './task-list';
+import { Group } from './group';
+import { TaskList, DEFAULT_LIST_NAME } from './task-list';
 
 export const TASKWALL_TYPE = {
   NORMAL: 'NORMAL'
-}
+};
 
 export class TaskWall {
   constructor(info) {
@@ -33,24 +29,25 @@ export class TaskWall {
 
   bundleCreate() {
     const self = this;
-    return new Promise(function(resolve, reject){
-      bookshelf.transaction(function(t){
-        self.model.save(null, {transacting: t})
-          .tap(function(taskWall){
-            Promise.all([
-              new Group({
-                taskWallId: taskWall.get('id'),
-                userId: taskWall.get('ownerId'),
-                accessLevel: 1
-              }).model.save(null, {transacting: t})
-            ]).then(() => {
+    return new Promise(function(resolve, reject) {
+      bookshelf.transaction(function(t) {
+        self.model.save(null, { transacting: t }).tap(function(taskWall) {
+          Promise.all([
+            new Group({
+              taskWallId: taskWall.get('id'),
+              userId: taskWall.get('ownerId'),
+              accessLevel: 1
+            }).model.save(null, { transacting: t })
+          ])
+            .then(() => {
               t.commit();
               resolve(taskWall);
-            }).catch(error => {
+            })
+            .catch(error => {
               t.rollback();
               reject(error);
             });
-          });
+        });
       });
     });
   }
