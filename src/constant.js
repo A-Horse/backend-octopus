109a0164e6de769665b85e575533f4e3a44dba19
fwchen <me@chenfangwei.com>
2019-01-23 00:10:19 +0800

    remove const

--- src/constant.js
+++ src/constant.js
@@ -4,10 +4,6 @@ export const tsApiPrefix = '/api/ts';
 export const tApiPrefix = '/api/t';
 export const wikiApiPrefix = '/api/wk';
 
-export const wikiServePort = 5505;
-export const taskServePort = 5503;
-export const userServePort = 5508;
+export const STORAGE_PATH = 'storage';
 
 export const JWT_KEY = 'jwts-token';
-
-export const STORAGE_PATH = 'storage';
