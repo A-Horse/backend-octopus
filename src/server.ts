1446548d5e7a9fa28f8d854452fb9d3acdf59f95
fwchen <me@chenfangwei.com>
2019-12-26 23:02:38 +0800

    add swagger

--- src/server.ts
+++ src/server.ts
@@ -4,8 +4,8 @@ import * as fs from 'fs';
 import * as http from 'http';
 import * as morgan from 'morgan';
 import * as path from 'path';
+import * as helmet from 'helmet';
 import rfs from 'rotating-file-stream';
-
 import { configure } from './config/configure';
 import { apiPrefix } from './constant';
 import { KanbanColumnRouter } from './domain/kanban-column/kanban-column-router';
@@ -15,51 +15,57 @@ import { ProjectRouter } from './domain/project/project-router';
 import { StatusErrorHandleMiddle } from './route/middle/error-handle';
 import { RootRouter } from './route/root';
 import { UserRouter } from './route/user.router';
+import { generateSwagger } from './util/swagger-helper';
 
-const helmet = require('helmet');
 
-const app = express();
+function initExpressApp(): express.Application {
+  const app = express();
 
-const logDirectory = path.join(__dirname, '../log/access');
+  const logDirectory = path.join(__dirname, '../log/access');
+  
+  if (!fs.existsSync(logDirectory)) {
+    fs.mkdirSync(logDirectory);
+  }
+  
+  const accessLogStream = rfs('access.log', {
+    interval: '1d',
+    path: logDirectory
+  });
+  
+  app.use(helmet());
+  app.use(
+    '/storage',
+    express.static('storage', {
+      maxAge: 1000 * 60 * 60 * 24 * 365
+    })
+  );
+  
+  app.use(morgan('combined'));
+  app.use(morgan('combined', { stream: accessLogStream }));
+  
+  app.use(require('body-parser').json());
+  app.use(require('body-parser').urlencoded({ extended: true }));
+  app.use(require('cookie-parser')());
+  
+  app.use(RootRouter);
+  
+  app.use('/api/user', UserRouter);
+  
+  app.use(apiPrefix, ProjectRouter);
+  app.use(apiPrefix, KanbanRouter);
+  app.use(apiPrefix, KanbanColumnRouter);
+  app.use(apiPrefix, ProjectIssueRouter);
+  
+  app.use(StatusErrorHandleMiddle);
 
-if (!fs.existsSync(logDirectory)) {
-  fs.mkdirSync(logDirectory);
+  return app;
 }
 
-const accessLogStream = rfs('access.log', {
-  interval: '1d',
-  path: logDirectory
-});
-
-app.use(helmet());
-app.use(
-  '/storage',
-  express.static('storage', {
-    maxAge: 1000 * 60 * 60 * 24 * 365
-  })
-);
-
-app.use(morgan('combined'));
-app.use(morgan('combined', { stream: accessLogStream }));
-
-app.use(require('body-parser').json());
-app.use(require('body-parser').urlencoded({ extended: true }));
-app.use(require('cookie-parser')());
-
-app.use(RootRouter);
-
-app.use('/api/user', UserRouter);
-
-app.use(apiPrefix, ProjectRouter);
-app.use(apiPrefix, KanbanRouter);
-app.use(apiPrefix, KanbanColumnRouter);
-app.use(apiPrefix, ProjectIssueRouter);
-
-app.use(StatusErrorHandleMiddle);
-
 export function startServer() {
+  const app = initExpressApp();
+  generateSwagger(app);
+
   const server = http.createServer(app);
   server.listen(configure.get('SERVE_PORT'), '0.0.0.0');
-
-  console.log(colors.green(`Octopus serve on http://0.0.0.0:5500`));
+  console.log(colors.green(`Octopus serve on http://0.0.0.0:${configure.get('SERVE_PORT')}`));
 }
---
+++ src/util/swagger-helper.ts
@@ -0,0 +1,30 @@
+import * as expressSwaggerGenerator from 'express-swagger-generator';
+import { Application } from 'express';
+
+export function generateSwagger(app: Application) {
+  const expressSwagger = expressSwaggerGenerator(app);
+  let options = {
+    swaggerDefinition: {
+      info: {
+        description: 'This is a sample server',
+        title: 'Swagger',
+        version: '1.0.0'
+      },
+      host: 'localhost:5500',
+      basePath: '/v1',
+      produces: ['application/json', 'application/xml'],
+      schemes: ['http', 'https'],
+      securityDefinitions: {
+        JWT: {
+          type: 'apiKey',
+          in: 'header',
+          name: 'Authorization',
+          description: ''
+        }
+      }
+    },
+    basedir: __dirname, //app absolute path
+    files: ['./routes/**/*.js'] //Path to the API handle folder
+  };
+  expressSwagger(options);
+}
