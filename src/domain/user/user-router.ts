44d9b2edb3ed2a328418244b2e8989870247695a
fwchen <me@chenfangwei.com>
2019-12-30 22:23:07 +0800

    implement get all user router

--- src/domain/user/user-router.ts
+++ src/domain/user/user-router.ts
@@ -1,16 +1,25 @@
 import * as express from 'express';
 import { authorizedRequestMiddle } from '../../route/middle/auth-handle.middle';
+import { UserRepositoryImpl } from './repository/impl/user-repository-impl';
+import { UserRepository } from './repository/user-repository';
 
 const UserRouter = express.Router();
 
 /**
+ * @typedef User
+ * @property {string} id.required
+ * @property {string} name.required
+ */
+
+/**
  * @route GET /users
  * @group user - Operations about user
- * @param {string} email.query.required - username or email - eg: user@domain
- * @param {string} password.query.required - user's password.
- * @returns {object} 200 - An array of user info
- * @returns {Error}  default - Unexpected error
+ * @returns {Array.<User>} 200 - An array of user
  */
-UserRouter.get('/users', authorizedRequestMiddle, async (req, res, next) => {});
+UserRouter.get('/users', authorizedRequestMiddle, async (req, res, next) => {
+    const userRepo: UserRepository = new UserRepositoryImpl();
+    const users = await userRepo.findAllUser();
+    res.json(users);
+});
 
 export { UserRouter };
