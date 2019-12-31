a4f53c0f8093682fe435b50f943bd1b81807d92e
fwchen <me@chenfangwei.com>
2019-12-31 10:37:02 +0800

    ignore some test path

--- jest.config.js
+++ jest.config.js
@@ -2,5 +2,6 @@ module.exports = {
   preset: 'ts-jest',
   testEnvironment: 'node',
   globalSetup: './test/setup.js',
-  globalTeardown: './test/teardown.js'
+  globalTeardown: './test/teardown.js',
+  testPathIgnorePatterns: ["/node_modules/", "/built/"]
 };
