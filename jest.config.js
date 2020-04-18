2007a4c2936a4d934b9d876cb4fffff72401781d
fwchen <me@chenfangwei.com>
2020-04-18 15:40:17 +0800

    config jest

--- jest.config.js
+++ jest.config.js
@@ -3,5 +3,6 @@ module.exports = {
   testEnvironment: 'node',
   globalSetup: './test/setup.js',
   globalTeardown: './test/teardown.js',
+  testTimeout: 50000,
   testPathIgnorePatterns: ["/node_modules/", "/built/"]
 };
